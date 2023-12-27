14dcece420b943e795ea31531fa55fca53abb18d
jivankesan <jkesan28@gmail.com>
2023-12-27 18:06:11 -0500

    updates

--- src/navigation/navigation/navigator_node.py
+++ src/navigation/navigation/navigator_node.py
@@ -6,13 +6,18 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__("first_node")
-        self.get_logger().info("Hello from ROS2")
+        self.counter_ = 0
+        self.create_timer(1.0, self.timer_callback)
+
+
+    def timer_callback(self):
+        self.get_logger().info("hello" + str(self.counter_))
+        self.counter_ +=1
 
 def main(args=None):
     rclpy.init(args=args)
     node = MyNode()
-    
-
+    rclpy.spin(node)
     rclpy.shutdown()
 
 if __name__ == "__main__":
--- src/navigation/setup.py
+++ src/navigation/setup.py
@@ -19,7 +19,8 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': [\
+            "navigator_node = navigation.navigator_node:main"
         ],
     },
 )
