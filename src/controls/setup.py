1d80328c9b5db427e51b2b7ab92aa5250d6e8eb7
ArjunMandairUWaterloo <absmanda@uwaterloo.ca>
2024-01-09 11:49:34 -0500

    Changed Ubuntu Version.

--- src/controls/setup.py
+++ src/controls/setup.py
@@ -1,15 +1,19 @@
+"""
+Setup file for Controls Package.
+"""
+
 from setuptools import find_packages, setup
 
-package_name = 'controls'
+PACKAGE_NAME = 'controls'
 
 setup(
-    name=package_name,
+    name=PACKAGE_NAME,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+            ['resource/' + PACKAGE_NAME]),
+        ('share/' + PACKAGE_NAME, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
