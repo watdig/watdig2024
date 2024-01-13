1c03cb08ca3e5ba5f1f6a2619fd5c86a26b6c7c7
JivanKesan <jkesan28@gmail.com>
2024-01-13 17:19:50 -0500

    added prm and more functionality

--- src/navigation/navigation/goal_manager.py
+++ src/navigation/navigation/goal_manager.py
@@ -1,48 +1,69 @@
+import math
+
 """
 Manages goals for the navigation package.
 """
+
+
 class GoalManager:
     """
     Initialization with no goal.
     """
+
     def __init__(self):
         # Initialize with no goal.
         self.current_goal = None
 
     def set_goal(self, float_one: float, float_two: float) -> None:
-        '''
-        Sets a new goal.
-        '''
+        """sets coordinate goal
+
+        Args:
+            float_one (float): _description_
+            float_two (float): _description_
+        """
         self.current_goal = (float_one, float_two)
         print(f"Current goal set: {self.current_goal}")
 
-    def update_goal(self, float_one: float, float_two: float) -> None:
-        """
-        Updates the current goal.
+    def update_goal(self, coordinate: tuple[float, float]) -> None:
+        """updates coordinate goal
+
+        Args:
+            coordinate (tuple[float, float]): _description_
         """
         if self.current_goal is not None:
-            self.current_goal = (float_one, float_two)
+            self.current_goal = coordinate
         else:
             print("No current goal, please use set_goal method first")
 
     def is_goal_reached(self, current_position: tuple[float, float]) -> bool:
-        """
-        Checks if the goal has been reached.
+        """checks whether current position is within tolerance of goal
+
+        Args:
+            current_position (tuple[float, float]): _description_
+
+        Returns:
+            bool: _description_
         """
         if self.current_goal is None:
             print("No current goal set")
             return False
 
-        if self.current_goal == current_position:
-            print(f"Target {self.current_goal} reached")
-            return True
+        radius = 1.1
 
-        return False
+        distance_squared = (self.current_goal[0] - current_position[0]) ** 2 + (
+            self.current_goal[1] - current_position[1]
+        ) ** 2
+
+        distance = math.sqrt(distance_squared)
+
+        return distance < radius
 
     def get_current_goal(self) -> tuple[float, float]:
-        '''
-        Returns the current goal.
-        '''
+        """returns the current goal
+
+        Returns:
+            tuple[float, float]: _description_
+        """
         if self.current_goal is None:
-            print('No goal set')
+            print("No goal set")
         return self.current_goal
