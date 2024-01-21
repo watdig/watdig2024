6612f27ae42f54730c27bcdcceb549f6da7ab081
JivanKesan <jkesan28@gmail.com>
2024-01-20 21:39:26 -0500

    changes

--- src/controls/controls/movement.py
+++ src/controls/controls/movement.py
@@ -7,34 +7,6 @@ class robot_action():
         self.front_left = ["pins"]
         self.back_right = ["pins"]
         self.back_left = ['pins']
-    
-    def turn_left(self, degree: float):
-        
-        #! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# This file presents an interface for interacting with the Playstation 4 Controller
-# in Python. Simply plug your PS4 controller into your computer using USB and run this
-# script!
-#
-# NOTE: I assume in this script that the only joystick plugged in is the PS4 controller.
-#       if this is not the case, you will need to change the class accordingly.
-#
-# Copyright © 2015 Clay L. McLeod <clay.l.mcleod@gmail.com>
-#
-# Distributed under terms of the MIT license.
-
-import os
-import pprint
-import pygame
-
-class PS4Controller(object):
-    """Class representing the PS4 controller. Pretty straightforward functionality."""
-
-    def init(self):
-        """Initialize the joystick components"""
-        pygame.init()
-        pygame.joystick.init()
         self.controller = pygame.joystick.Joystick(0)
         self.controller.init()
         self.axis_data = False
