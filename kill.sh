3c1a567d61426df71300b5d48ee378edab8ad012
ArjunMandairUWaterloo <absmanda@uwaterloo.ca>
2024-03-09 16:23:57 -0500

    Created Kill and Cleanbuild Shell Scripts.

---
+++ kill.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Source the setup file
+source install/setup.bash
+
+# Define the list of node names
+NODES=("csv_parse")
+
+# Check if there are any nodes running
+if [ ${#NODES[@]} -gt 0 ]; then
+    # Iterate through each node, get its PID, and kill it
+    for NODE in "${NODES[@]}"; do
+        # Get the PID of the node
+        NODE_PID=$(ps -e | grep $NODE | awk '{print $1}')
+        
+        if [ -n "$NODE_PID" ]; then
+            # Kill the node using its PID
+            kill $NODE_PID
+            echo "Node $NODE (PID: $NODE_PID) killed."
+        else
+            echo "Failed to retrieve PID for node $NODE."
+        fi
+    done
+else
+    echo "No nodes running."
+fi
--- start.sh
+++ start.sh
@@ -1,19 +1,7 @@
-# Remove all built directories
-echo "Removing ROS Directories."
-rm -r ./build &
-rm -r ./log &
-rm -r ./install &
-
-# Wait for removes to complete
-wait
-echo "ROS Directories removed successfully."
-
-# Rebuilding ROS Directories and Sourcing setup
-echo "Building ROS Directories"
-colcon build
-echo "Colcon Build Succesful. ROS Directories Recreated"
-source install/setup.bash
+# Clean Build ROS Packages
+./cleanbuild.sh
 
 # Starting ROS Nodes
+source install/setup.bash
 ros2 run communication csv_parse &
 rqt_graph
