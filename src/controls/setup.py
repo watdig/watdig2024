46bc5b478e2c6701766424cf5a03bd683088c2a6
jivankesan <jkesan28@gmail.com>
2024-03-09 20:24:30 -0500

    updates to controls

--- src/controls/setup.py
+++ src/controls/setup.py
@@ -23,7 +23,9 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
+        'console_scripts': [\
+            "controls_node = controls.controller_node:main",
+            "action_server = scripts.turn_and_move_action_server:main"
         ],
     },
 )
