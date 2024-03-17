22f6d6f39db5b84d6ee9e2933ec6f548e75e7d7e
jivankesan <jkesan28@gmail.com>
2024-03-17 18:27:19 -0400

    changes

--- start.sh
+++ start.sh
@@ -8,4 +8,6 @@ ros2 run navigation navigator_node &
 ros2 run controls controls_node &
 ros2 run controls action_server &
 ros2 run localization localization_node &
+ros2 run sensor_integration back_uwb_node &
+ros2 run sensor_integration front_uwb_node &
 rqt_graph
