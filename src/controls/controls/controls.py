41ff19da3e0a01d4781cdcb11b3cebb63c44e764
Kish16 <78311114+Kish16@users.noreply.github.com>
2024-03-23 00:40:56 -0400

    Update controls.py

--- src/controls/controls/controls.py
+++ src/controls/controls/controls.py
@@ -4,22 +4,20 @@ import time
 class Car():
     def __init__(self):
         # Define GPIO pins for motors
-        self.PWM_RB = 16  
-        self.DIR_RB = 12
         
         self.PWM_LB = 6
         self.DIR_LB = 5
         
-        self.PWM_RF = 27
-        self.DIR_RF = 17
+        self.PWM_R = 27
+        self.DIR_R = 17
         
         self.PWM_LF = 23
         self.DIR_LF = 24
 
-        self.pins = [self.PWM_RF, self.DIR_RF, self.PWM_LB, self.DIR_LB, self.PWM_RB, self.DIR_RB, self.PWM_LF, self.DIR_LF]
-        self.pwm_pins = [self.PWM_LB, self.PWM_LF, self.PWM_RB, self.PWM_RF]
+        self.pins = [self.PWM_R, self.DIR_R, self.PWM_LB, self.DIR_LB, self.PWM_LF, self.DIR_LF]
+        self.pwm_pins = [self.PWM_LB, self.PWM_LF, self.PWM_R]
         self.dir_L = [self.DIR_LB, self.DIR_LF]
-        self.dir_R = [self.DIR_RB, self.DIR_RF]
+        self.dir_R = [self.DIR_R]
  
 
         # Setup GPIO pins
