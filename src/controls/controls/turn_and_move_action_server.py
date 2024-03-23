b79e56ef4de2f0558f52cdea889cdd47d227b60f
JivanKesan <jkesan28@gmail.com>
2024-03-22 22:06:14 -0400

    updated movement server with gyroscope subscriber

--- src/controls/controls/turn_and_move_action_server.py
+++ src/controls/controls/turn_and_move_action_server.py
@@ -6,6 +6,7 @@ from rclpy.node import Node
 from controls.controls import Car
 from controls.encoder import reader
 from action_folder.action import TurnAndMove 
+from interfaces.msg import CurrentCoords
 
 from std_msgs.msg import String 
 
@@ -21,9 +22,18 @@ class TurnAndMoveActionServer(Node):
         self.pi = pigpio.pi()
         self.p = reader(self.pi, self.pin1)
         self.Car = Car()
+        self.current_gyro
         
         self.current_action_publisher = self.create_publisher(String, 'current_action', 10)
+        self.subscription_current_location = self.create_subscription(CurrentCoords,
+            'current_location_topic', self.current_location_callback, 10)
 
+    def current_location_callback(self, msg):
+        """
+        Subscription Node that subscribes to the Localization node. Calls the publish_next_direction method.
+        """
+        self.current_gyro = msg.angle
+        
     async def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
         feedback_msg = TurnAndMove.Feedback()
@@ -34,10 +44,6 @@ class TurnAndMoveActionServer(Node):
         
         self.current_action_publisher.publish(String(data="turning"))
         
-        # adjust this for gyroscope once it comes in
-        seconds_per_degree = 1.947 / 90  # Time it takes to turn one degree
-        turn_duration = abs(angle) * seconds_per_degree
-
         # Turn based on angle
         if goal_handle.request.angle > 0:
             self.car.turn_right()
@@ -45,9 +51,8 @@ class TurnAndMoveActionServer(Node):
             self.car.turn_left()
 
         # Wait for the duration of the turn, non-blocking wait
-        end_time = self.get_clock().now() + rclpy.duration.Duration(seconds=turn_duration)
-        while self.get_clock().now() < end_time:
-            await rclpy.task.sleep(0.1)
+        while (self.current_gyro - angle) > 2:
+            pass
             
         self.current_action_publisher.publish(String(data="driving"))    
         
