518b03b50c7a8f57604ffc1bd5333a881199e7f8
JivanKesan <jkesan28@gmail.com>
2024-03-23 22:12:18 -0400

    changes

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ ros2 run communication position_subscriber &
 ros2 run sensor_integration gyro_node &
 #ros2 run controls controls_node &
 #ros2 run controls action_server &
-#ros2 run localization localization_node &
-#ros2 run sensor_integration front_uwb_node &
+ros2 run localization localization_node &
+ros2 run sensor_integration front_uwb_node &
 rqt_graph
