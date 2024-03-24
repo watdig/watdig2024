27fd5c5343cd08973b401bc45e64f21935268e3b
JivanKesan <jkesan28@gmail.com>
2024-03-23 20:14:01 -0400

    updates

--- startonly.sh
+++ startonly.sh
@@ -1,4 +1,7 @@
 source install/setup.bash
 ros2 run communication csv_parse &
 ros2 run navigation navigator_node &
+ros2 run sensor_integration front_uwb_node &
+ros2 run sensor_integration gyro_node &
+ros2 run localization localization_node &
 rqt_graph
