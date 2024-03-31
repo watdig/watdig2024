f73435a81bb6ce754499ab5942c974b307dc7c5c
jivankesan <jkesan28@gmail.com>
2024-03-30 22:54:38 -0400

    reverted nav, locali, awb to 9:15 revision

--- src/sensor_integration/sensor_integration/front_uwb.py
+++ src/sensor_integration/sensor_integration/front_uwb.py
@@ -8,7 +8,7 @@ class FrontUWB(Node):
     def __init__(self):
         super().__init__('front_uwb_node')
         self.front_uwb_publisher = self.create_publisher(Float32MultiArray, 'front_uwb_topic', 10)
-        # self.serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)        
+        self.serial_port = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.1)        
         self.create_timer(0.1, self.publish_uwb) 
 
     def read_from_serial(self):
@@ -19,25 +19,20 @@ class FrontUWB(Node):
             if data:
                 data = data.split(",")
                 anchor_id = int(data[0])
-                self.get_logger().info(f"anchor_id: {anchor_id}")
                 distance = float(data[1])
                 uwb_distances_dict[anchor_id] = distance
         return uwb_distances_dict
             
                             
     def publish_uwb(self):
-        dictionary2 = {1: 0.0, 2: 0.0}
-        # dictionary = self.read_from_serial()
-        #  uncomment the line above if you want to run with the uwbs
-        dictionary = {}
-        msg = Float32MultiArray()
+        dictionary = self.read_from_serial()
         if dictionary:
+            msg = Float32MultiArray()
             msg.data = [dictionary.get(1, 0.0), dictionary.get(2, 0.0), dictionary.get(3, 0.0), dictionary.get(4, 0.0)]
             self.front_uwb_publisher.publish(msg)
             # self.get_logger().info(f'Published uwbs: {msg.data}')
         else:
-            msg.data = [dictionary2[1], dictionary2[2]]
-            # self.get_logger().warn('dictionary is empty')
+            self.get_logger().info('dictioanry is empty')
 
 def main(args=None):
     rclpy.init(args=args)
