cbf001672e3ec1bfce1324edfd009406a502fc33
Kish16 <kishanmuhundhan@gmail.com>
2024-03-30 20:45:56 -0400

    localizaion node changes

--- src/navigation/navigation/navigator_node.py
+++ src/navigation/navigation/navigator_node.py
@@ -288,7 +288,7 @@ class NavigatorNode(Node):
         
         def distance(point1, point2):
             #linear distance modifier (can modify between runs)
-            modifier = 1.0
+            modifier = 0.01
             dist = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
             return dist*modifier
     
