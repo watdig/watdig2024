b0f959ed187f6ce1641e8cb5453f3d8f72bd31bd
jivankesan <jkesan28@gmail.com>
2024-03-30 22:40:34 -0400

    changes

--- src/sensor_integration/sensor_integration/front_uwb.py
+++ src/sensor_integration/sensor_integration/front_uwb.py
@@ -27,7 +27,7 @@ class FrontUWB(Node):
                             
     def publish_uwb(self):
         dictionary2 = {1: 0.0}
-        # dictionary = self.read_from_serial()
+        dictionary = self.read_from_serial()
         #  uncomment the line above if you want to run with the uwbs
         dictionary = {}
         msg = Float32MultiArray()
